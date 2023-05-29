db435fb4a4b8640e29954530a159be85b27c4f40
feng <103472156+fengyao33@users.noreply.github.com>
2023-05-29 23:50:54 +0800

    theater api

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -14,6 +14,7 @@ const HeaderStyle = styled(Header)`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  background-color: #7e7e7e;
 `;
 
 const UserBtn = styled(Button)`
