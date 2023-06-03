1b1d8a0a13da0b769b13989dbc6558a4229108af
Leonard <clps935419@gmail.com>
2023-06-03 17:23:50 +0800

    feat:拖曳基本功能

---
+++ src/pages/moviesDrag/index.js
@@ -0,0 +1,250 @@
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
+import styled from "styled-components";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { StrictModeDroppable } from "./components/StrictModeDroppable";
+
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+  return result;
+};
+
+const copy = (source, destination, droppableSource, droppableDestination) => {
+  const sourceClone = Array.from(source);
+  const destClone = Array.from(destination);
+  const item = sourceClone[droppableSource.index];
+  destClone.splice(droppableDestination.index, 0, { ...item, id: uuid() });
+  return destClone;
+};
+
+const move = (source, destination, droppableSource, droppableDestination) => {
+  const sourceClone = Array.from(source);
+  const destClone = Array.from(destination);
+  const [removed] = sourceClone.splice(droppableSource.index, 1);
+  destClone.splice(droppableDestination.index, 0, removed);
+  return {
+    ...sourceClone,
+    ...destClone,
+  };
+};
+
+const Content = styled.div`
+  margin-right: 200px;
+`;
+
+const Item = styled.div`
+  display: flex;
+  user-select: none;
+  padding: 0.5rem;
+  margin: 0 0 0.5rem 0;
+  align-items: flex-start;
+  align-content: flex-start;
+  line-height: 1.5;
+  border-radius: 3px;
+  background: #fff;
+  border: 1px ${(props) => (props.isDragging ? "dashed #4099ff" : "solid #ddd")};
+`;
+
+const Clone = styled(Item)`
+  + div {
+    display: none !important;
+  }
+`;
+
+const Handle = styled.div`
+  display: flex;
+  align-items: center;
+  align-content: center;
+  user-select: none;
+  margin: -0.5rem 0.5rem -0.5rem -0.5rem;
+  padding: 0.5rem;
+  line-height: 1.5;
+  border-radius: 3px 0 0 3px;
+  background: #fff;
+  border-right: 1px solid #ddd;
+  color: #000;
+`;
+
+const List = styled.div`
+  border: 1px
+    ${(props) => (props.isDraggingOver ? "dashed #000" : "solid #ddd")};
+  background: #fff;
+  padding: 0.5rem 0.5rem 0;
+  border-radius: 3px;
+  flex: 0 0 150px;
+  font-family: sans-serif;
+`;
+
+const Kiosk = styled(List)`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  width: 200px;
+`;
+
+const Container = styled(List)`
+  margin: 0.5rem 0.5rem 1.5rem;
+  background: #ccc;
+  min-height: 300px;
+`;
+
+const Notice = styled.div`
+  display: flex;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
+  padding: 0.5rem;
+  margin: 0 0.5rem 0.5rem;
+  border: 1px solid transparent;
+  line-height: 1.5;
+  color: #aaa;
+`;
+
+const ITEMS = [
+  { id: "item-1", content: "Item 1" },
+  { id: "item-2", content: "Item 2" },
+  { id: "item-3", content: "Item 3" },
+];
+
+const App = () => {
+  const [state, setState] = useState({ [uuid()]: []});
+
+  const onDragEnd = (result) => {
+    const { source, destination } = result;
+
+    if (!destination) {
+      return;
+    }
+    console.log("sour", source.droppableId);
+    switch (source.droppableId) {
+      case destination.droppableId:
+        console.log("---", destination.droppableId);
+        setState((prevState) => ({
+          ...prevState,
+          [destination.droppableId]: reorder(
+            prevState[source.droppableId],
+            source.index,
+            destination.index
+          ),
+        }));
+        break;
+      case "ITEMS":
+        console.log("***", ITEMS);
+
+        setState((prevState) => ({
+          ...prevState,
+          [destination.droppableId]: copy(
+            ITEMS,
+            prevState[destination.droppableId],
+            source,
+            destination
+          ),
+        }));
+        break;
+      default:
+        console.log('default')
+        setState((prevState) =>
+          move(
+            prevState[source.droppableId],
+            prevState[destination.droppableId],
+            source,
+            destination
+          )
+        );
+        break;
+    }
+  };
+
+  const addList = () => {
+    setState((prevState) => ({ ...prevState, [uuid()]: [] }));
+  };
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <StrictModeDroppable droppableId="ITEMS" isDropDisabled={true}>
+        {(provided, snapshot) => (
+          <Kiosk
+            ref={provided.innerRef}
+            isDraggingOver={snapshot.isDraggingOver}
+          >
+            {ITEMS.map((item, index) => (
+              <Draggable key={item.id} draggableId={item.id} index={index}>
+                {(provided, snapshot) => (
+                  <React.Fragment>
+                    <Item
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      isDragging={snapshot.isDragging}
+                      style={provided.draggableProps.style}
+                    >
+                      {item.content}
+                    </Item>
+                    {snapshot.isDragging && <Clone>{item.content}</Clone>}
+                  </React.Fragment>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </Kiosk>
+        )}
+      </StrictModeDroppable>
+      <Content>
+        <button onClick={addList}>
+          <svg width="24" height="24" viewBox="0 0 24 24">
+            <path
+              fill="currentColor"
+              d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z"
+            />
+          </svg>
+          Add List
+        </button>
+        {Object.keys(state).map((list) => (
+          <StrictModeDroppable key={list} droppableId={list}>
+            {(provided, snapshot) => (
+              <Container
+                ref={provided.innerRef}
+                isDraggingOver={snapshot.isDraggingOver}
+              >
+                {state[list].length
+                  ? state[list].map((item, index) => (
+                      <Draggable
+                        key={item.id}
+                        draggableId={item.id}
+                        index={index}
+                      >
+                        {(provided, snapshot) => (
+                          <Item
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            isDragging={snapshot.isDragging}
+                            style={provided.draggableProps.style}
+                          >
+                            <Handle {...provided.dragHandleProps}>
+                              <svg width="24" height="24" viewBox="0 0 24 24">
+                                <path
+                                  fill="currentColor"
+                                  d="M3,15H21V13H3V15M3,19H21V17H3V19M3,11H21V9H3V11M3,5V7H21V5H3Z"
+                                />
+                              </svg>
+                            </Handle>
+                            {item.content}
+                          </Item>
+                        )}
+                      </Draggable>
+                    ))
+                  : !provided.placeholder && <Notice>Drop items here</Notice>}
+                {provided.placeholder}
+              </Container>
+            )}
+          </StrictModeDroppable>
+        ))}
+      </Content>
+    </DragDropContext>
+  );
+};
+
+export default App;
